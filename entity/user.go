6210673443772590c40859f8075598ee69bf89d1
Evgeniy <spkissaal@gmail.com>
2025-01-23 22:57:48 +0500

    Refactoring into microservices

---
+++ entity/user.go
@@ -0,0 +1,40 @@
+package entity
+
+import (
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"pushpost/internal/services/user_service/domain/dto"
+	"time"
+)
+
+type User struct {
+	gorm.Model
+	UUID              uuid.UUID
+	Name              string
+	Email             string `gorm:"unique"`
+	Password          string
+	Age               uint
+	LastTimeActivity  time.Time
+	IsEmailVerified   bool
+	VerificationToken string
+}
+
+func NewUser(dto dto.RegisterUserDTO) (*User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(dto.Password), bcrypt.DefaultCost)
+	if err != nil {
+
+		return nil, err
+	}
+
+	return &User{
+		UUID:              uuid.New(),
+		Name:              dto.Name,
+		Email:             dto.Email,
+		Password:          string(hashedPassword),
+		Age:               dto.Age,
+		IsEmailVerified:   false,
+		VerificationToken: uuid.New().String(),
+	}, nil
+
+}
---
+++ storage/repository/user_repository.go
@@ -0,0 +1,60 @@
+package repository
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"pushpost/internal/services/user_service/entity"
+)
+
+type UserRepository struct {
+	DB *gorm.DB
+}
+
+func (r *UserRepository) RegisterUser(user *entity.User) error {
+	return r.DB.Create(&user).Error
+}
+
+func (r *UserRepository) GetUserByEmail(email string) (*entity.User, error) {
+	var user entity.User
+	if err := r.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) GetUserByUUID(uuid uuid.UUID) (*entity.User, error) {
+	var user entity.User
+	if err := r.DB.Where("uuid = ?", uuid).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) AddFriend(userUUID, friendEmail string) error {
+
+	user, err := r.GetUserByUUID(userUUID)
+	if err != nil {
+		return err
+	}
+	friend, err := r.GetUserByUUID(friendUUID)
+	if err != nil {
+		return err
+	}
+	//Check if friendship already exists in either direction
+	var existingFriendship entity.Friendship
+	result := r.DB.Where(
+		"(user_id = ? AND friend_id = ?) OR (user_id = ? AND friend_id = ?)",
+		user.ID, friend.ID, friend.ID, user.ID,
+	).First(&existingFriendship)
+
+	if result.Error == nil {
+		return fmt.Errorf("friendship already exists")
+	}
+
+	if result.Error != gorm.ErrRecordNotFound {
+		return result.Error
+	}
+	friendship := entity.Friendship{UserID: user.ID, FriendID: friend.ID}
+	return r.DB.Create().Error
+}
